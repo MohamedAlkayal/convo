8f621986912e20edaa3014cb061bc28d4db53f8b
MohamedAlkayal <mohamedalkayal97@gmail.com>
2024-03-16 16:57:31 +0200

    Installed essential packages and created the main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
